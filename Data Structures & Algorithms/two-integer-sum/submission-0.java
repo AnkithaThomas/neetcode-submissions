59696b5406a19ac3f7a1bc61610e01743d4b4752
Ankitha Shaji Thomas <159093067+AnkithaThomas@users.noreply.github.com>
2026-08-31 10:36:33 -0500

    Bulk sync: 14 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    ans[0] = Math.min(i,j);
+                    ans[1] = Math.max(i,j);
+                }
+            }
+        }
+        return ans;
+            
+    }
+}
